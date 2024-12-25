e66596bc08401e5e4eb833a9493b196a1726e815
Leonardo Andrade <leoandrade1598@gmail.com>
2024-12-25 20:05:11 -0300

    Exercícios - Capítulo 02

---
+++ cap-02/js/resp02_c.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+    const produto = frm.inProduto.value
+    const preco = frm.inPreco.value
+
+    const total = (2 * preco) + preco * 0.50
+    const desconto =  preco * 0.50
+
+    resp1.innerText = `${produto} - Promoção: Leve 3 por R$ ${total.toFixed(2)}`
+    resp2.innerText = `O 3º produto custa apenas R$: ${desconto.toFixed(2)}`
+
+    e.preventDefault()
+})
