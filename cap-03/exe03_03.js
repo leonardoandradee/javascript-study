5eba0086c40185d0cef884fa36fad3ac51eb30ac
Leonardo Andrade <leoandrade1598@gmail.com>
2025-02-22 10:53:31 -0300

    Voltando aos estudos

--- cap-03/exe03_03.js
+++ cap-03/exe03_03.js
@@ -3,5 +3,5 @@ const salario = Number(prompt("Salário R$: "))
 const tempo = Number(prompt("Tempo (anos): "))
 const quadrienios = Math.floor(tempo / 4)
 const acrescimo = salario * quadrienios / 100
-/* console.log(`Quadriênios: ${quadrienios}`) */
+console.log(`Quadriênios: ${quadrienios}`)
 console.log(`Salário Final R$: ${(salario+acrescimo).toFixed(2)}`)
